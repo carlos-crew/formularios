8c403492d75a1f1aa4d830a0b040a47b9dc6b4b6
carlos-crew <66502581+carlos-crew@users.noreply.github.com>
2022-09-30 16:54:35 -0500

    Formularios

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mt-3">
+   <Header />
+
+
+    <Formulario />
+  </div>>
+</template>
+
+<script>
+import Header from "./componentes/Header.vue";
+import Formulario from "./componentes/Formulario.vue";
+
+export default {
+  components: {
+    Header,
+    Formulario
+  }
+}
+</script>
+
+
